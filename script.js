dfba7ad6c18f3477c62705b69f08808ee54506af
erlams <elmaddin@gmail.com>
2023-11-18 19:11:21 +0100

    fff

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let weather = prompt('WHAT IS THE TEMPERATURE');
 
 let netice;
-if (weather > 15 ) {
-netice = 'NORMAL TEMPERATURE';
+if (weather > 15) {
+    netice = 'NORMAL TEMPERATURE';
 
-}else {
+} else {
     netice = 'COLD COLD COLD';
 }
 document.getElementById('wet').innerHTML = netice;
